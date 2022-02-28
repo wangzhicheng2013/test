a343a4a8cabc0307fc4eddfbc302e605ed27f7e9
wangzhicheng2013 <2363702560@qq.com>
2022-02-28 13:32:57 +0800

    2

--- test.cpp
+++ test.cpp
@@ -1,6 +1,8 @@
 #include <boost/tuple/tuple.hpp>
+#include <boost/tuple/tuple_comparsion.hpp>
 #include <iostream>
 #include <string>
+#include <cassert>
 boost::tuple<int, std::string>almost_a_pair(10, "Hello");
 boost::tuple<int, float, double, int>quad(10, 1.0f, 10.0d, 1);
 void fun0() {
@@ -11,8 +13,17 @@ void fun0() {
     std::cout << "str = " << str << std::endl;
     std::cout << "d = " << d << std::endl;
 }
+void fun1() {
+    std::set<boost::tuple<int, double, int>>s;
+    s.insert(boost::make_tuple(1, 1.0d, 2));
+    s.insert(boost::make_tuple(2, 10.0d, 2));
+    s.insert(boost::make_tuple(3, 100.0d, 2));
+    const auto t = boost::make_tuple(0, -1.0, 2);
+    assert(2 == boost::get<2>(t));
+}
 int main() {
     fun0();
-    
+    fun1();
+
     return 0;
 }
