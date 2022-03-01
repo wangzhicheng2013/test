9fe455ff4de79ce6c8baabba753d442962657a35
wangzhicheng2013 <2363702560@qq.com>
2022-03-01 11:09:08 +0800

    7

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+struct bit1 {
+    unsigned a:20;
+    unsigned b:6;
+    unsigned c:2;
+}test_bit1;
+struct bit2 {
+    unsigned d:20;
+    unsigned e:6;
+    unsigned f:20;
+}test_bit2;
+int main() {
+    printf("%zd\n", sizeof(test_bit1));
+    printf("%zd\n", sizeof(test_bit2));
+    test_bit1.a = 0;
+    test_bit1.b = 0;
+    test_bit1.c = 3;
+    printf("%d\n", test_bit2.c);
+    printf("%d\n", test_bit1);
+    
+    return 0;
+}
