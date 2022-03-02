01e235144e0a7e5c0a21c5c982998ad574ed7739
wangzhicheng2013 <2363702560@qq.com>
2022-03-02 13:40:04 +0800

    9

--- test.c
+++ test.c
@@ -1,39 +1,13 @@
 #include <stdio.h>
-struct bit1 {
-    unsigned a:20;
-    unsigned b:6;
-    unsigned c:2;
-}test_bit1;
-struct bit2 {
-    unsigned d:20;
-    unsigned e:6;
-    unsigned f:20;
-}test_bit2;
-union UN {
-    unsigned int u;
-    struct {
-        unsigned char a:1;
-        unsigned char b:2;
-        unsigned char c:4;
-        unsigned char d:8;
-    } ST;
-};
-
+typedef char * PSTR;
 int main() {
-    printf("%zd\n", sizeof(test_bit1));
-    printf("%zd\n", sizeof(test_bit2));
-    test_bit1.a = 0;
-    test_bit1.b = 0;
-    test_bit1.c = 3;
-    printf("%d\n", test_bit1.c);
-
-    union UN un;
-    un.u = 0;
-    un.ST.a = 1;
-    un.ST.b = 2;
-    un.ST.c = 4;
-    un.ST.d = 8;
-    printf("%0x\n", un.u);
+    char string[4] = "abc";
+    const char *p1 = string;
+    const PSTR p2 = string;
+    p1++;
+    p2++;
+    *p2 = 'd';
+    printf("%s\n", string);
     
     return 0;
 }
