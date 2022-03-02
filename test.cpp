ae35de29ddff349c443c6485ef2426f87469ff47
wangzhicheng2013 <2363702560@qq.com>
2022-03-02 13:19:32 +0800

    8

--- test.cpp
+++ test.cpp
@@ -1,39 +1,15 @@
 #include <boost/tuple/tuple.hpp>
-#include <boost/tuple/tuple_comparison.hpp>
 #include <iostream>
-#include <string>
-#include <set>
-#include <cassert>
-boost::tuple<int, std::string>almost_a_pair(10, "Hello");
-boost::tuple<int, float, double, int>quad(10, 1.0f, 10.0d, 1);
-void fun0() {
-    auto i = boost::get<0>(almost_a_pair);
-    auto &str = boost::get<1>(almost_a_pair);
-    double d = boost::get<2>(quad);
-    std::cout << "i = " << i << std::endl;
-    std::cout << "str = " << str << std::endl;
-    std::cout << "d = " << d << std::endl;
-}
-void fun1() {
-    std::set<boost::tuple<int, double, int>>s;
-    s.insert(boost::make_tuple(1, 1.0d, 2));
-    s.insert(boost::make_tuple(2, 10.0d, 2));
-    s.insert(boost::make_tuple(3, 100.0d, 2));
-    const auto t = boost::make_tuple(0, -1.0, 2);
-    assert(2 == boost::get<2>(t));
-}
-void fun2() {
-    boost::tuple<int, double>tt(1, 10.00d);
-    int a = 0;
-    double b = 0;
-    boost::tie(a, b) = tt;
-    std::cout << "a = " << a << std::endl;
-    std::cout << "b = " << b << std::endl;
-}
+template <int I>
+struct printer {
+    printer() {
+        std::cout << I << std::endl;
+    }
+};
+
 int main() {
-    fun0();
-    fun1();
-    fun2();
+    boost::tuple<printer<0>, printer<1>, printer<2>>t;
+    (void)t;
 
     return 0;
 }
