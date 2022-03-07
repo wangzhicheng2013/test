172a777fe987104f85ac9e4b1fb71a849716e3e8
wangzhicheng2013 <2363702560@qq.com>
2022-03-07 09:44:23 +0800

    19

--- test.cpp
+++ test.cpp
@@ -1,45 +1,31 @@
-#include <boost/bind/bind.hpp>
-#include <boost/array.hpp>
+#include <boost/type_index.hpp>
 #include <iostream>
-#include <string>
-#include <algorithm>
-#include <functional>
-#include <cassert>
+#include <typeinfo>
+template <class T>
+void do_something(const T &x) {
+    std::cout << "T is " << typeid(T).name() << std::endl;
+    (void)x;
+}
 void sample1() {
-    const boost::array<int, 12> v = {
-         1, 2, 3, 4, 5, 6, 7, 100, 99, 98, 97, 96
-    };
-    const std::size_t count0 = std::count_if(v.begin(), v.end(), [] (int x) { return 5 < x; } );
-    const std::size_t count1 = std::count_if(v.begin(), v.end(), boost::bind(std::less<int>(), 5, boost::placeholders::_1) );
-    assert(count0 == count1);
+    auto &&x = 42;
+    std::cout << "x is " << typeid(decltype(x)).name() << std::endl;
+    (void)x;
+}
+template <class T>
+void do_something_again(const T &x) {
+    std::cout << "T is " << boost::typeindex::type_id<T>() << std::endl;
+    (void)x;
 }
 void sample2() {
-    const boost::array<std::string, 3> v = {
-          "We ", "are", " the champions!"
-    };
-    const std::size_t count0 = std::count_if(v.begin(), v.end(), [] (const std::string &str) { return str.empty(); } );
-    const std::size_t count1 = std::count_if(v.begin(), v.end(), boost::bind(&std::string::empty, boost::placeholders::_1) );
-    assert(count0 == count1);
-}
-void sample3() {
-    const boost::array<std::string, 3> v = {
-          "We ", "are", " the champions!"
-    };
-    const std::size_t count0 = std::count_if(v.begin(), v.end(), [] (const std::string &str) { return str.size() < 5; } );
-    const std::size_t count1 = std::count_if(v.begin(), v.end(), boost::bind(std::less<std::size_t>(), boost::bind(&std::string::size, boost::placeholders::_1), 5) );
-    assert(count0 == count1);
-}
-void sample4() {
-    const auto twice = boost::bind(std::plus<int>(), boost::placeholders::_1, boost::placeholders::_1);
-    assert(twice(2) == 4);
-    const auto minus_from_second = boost::bind(std::minus<int>(), boost::placeholders::_2, boost::placeholders::_1);
-    assert(minus_from_second(2, 4) == 2);
+    auto &&x = 42;
+    std::cout << "x is " << boost::typeindex::type_id_with_cvr<decltype(x)>() << std::endl;
+    (void)x;
 }
 int main() {
+    do_something(1.0);
     sample1();
+    do_something_again(2.0);
     sample2();
-    sample3();
-    sample4();
 
     return 0;
 }
