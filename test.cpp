4a2cb5848b6bbb5553a72f60da5ce84ddd312731
wb7898 <wb7898@arcsoft.com.cn>
2022-03-19 14:36:43 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,13 +1,17 @@
-#include <iostream>
-#include <boost/version.hpp>
-#include <boost/config.hpp>
+class descriptor_owner_fixed : private boost::noncopyable {
+public:
+    descriptor_owner_fixed(const char *param) {
+        (void)param;
+    }
+};
+void i_am_good() {
+    descriptor_owner_fixed d1("OOP");
+    descriptor_owner_fixed d2("OOC");
+}
+
 
 int main() {
-    std::cout << BOOST_VERSION << std::endl;
-    std::cout << BOOST_LIB_VERSION << std::endl;
-    std::cout << BOOST_PLATFORM << std::endl;
-    std::cout << BOOST_COMPILER << std::endl;
-    std::cout << BOOST_STDLIB << std::endl;
-
+    i_am_good();
+    
     return 0;
 }
