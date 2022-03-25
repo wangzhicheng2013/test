c38143240db415d88b208905a31b9749b74d0fad
wb7898 <wb7898@arcsoft.com.cn>
2022-03-25 15:13:17 +0800

    8

--- test.cpp
+++ test.cpp
@@ -4,6 +4,8 @@
 #include <cassert>
 #include "boost/noncopyable.hpp"
 #include "boost/config.hpp"
+#include "boost/move/move.hpp"
+#include "boost/container/vector.hpp"
 class descriptor_owner : private boost::noncopyable {
     void *descriptor_;
 public:
@@ -60,6 +62,36 @@ public:
         clear();
     }
 };
+class descriptor_owner_movable {
+    void *descriptor_;
+    BOOST_MOVABLE_BUT_NOT_COPYABLE(descriptor_owner_movable);
+public:
+    descriptor_owner_movable() : descriptor_(nullptr) {}
+    explicit descriptor_owner_movable(const char *param) : descriptor_(strdup(param)) {}
+    descriptor_owner_movable(BOOST_RV_REF(descriptor_owner_movable)param) BOOST_NOEXCEPT : descriptor_(param.descriptor_) {}
+        param.descriptor_ = nullptr;
+    }
+    descriptor_owner_movable & operator = (descriptor_owner_movable)param) BOOST_NOEXCEPT {
+        descriptor_owner_movable tmp(std::move(param));
+        std::swap(descriptor_, tmp.descriptor_);
+        return *this;
+    }
+    void clear() {
+        if (descriptor_) {
+            free(descriptor_);
+            descriptor_ = nullptr;
+        }
+    }
+    bool empty() const {
+        return !descriptor_;
+    }
+    ~descriptor_owner_movable() BOOST_NOEXCEPT {
+        clear();
+    }
+};
+descriptor_owner_movable construct_descriptor3() {
+    return descriptor_owner_movable("Construct using this string");
+}
 int main() {
     descriptor_owner ret;
     construct_descriptor1(ret);
@@ -69,5 +101,11 @@ int main() {
     desc = descriptor_owner1("Construct using this string");
     assert(!desc.empty());
 
+    descriptor_owner_movable movable;
+    movable = construct_descriptor3();
+
+    boost::container::vector<descriptor_owner_movable>vec;
+    vec.resize(10);
+    vec.push_back(construct_descriptor3());
     return 0;
 }
