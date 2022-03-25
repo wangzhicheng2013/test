4a5286dc0a427d27b0a810277e103dee19cc90e2
wb7898 <wb7898@arcsoft.com.cn>
2022-03-25 14:16:24 +0800

    2

--- test.cpp
+++ test.cpp
@@ -11,6 +11,11 @@ public:
     void swap(descriptor_owner& desc) {
         std::swap(descriptor_, desc.descriptor_);
     }
+    void show() const {
+        if (descriptor_) {
+            const char *ss = (const char *)descriptor_;
+        std::cout << ss << std::endl;
+    }
     ~descriptor_owner() {
         if (descriptor_) {
             free(descriptor_);
@@ -24,7 +29,7 @@ void construct_descriptor1(descriptor_owner& ret) {
 int main() {
     descriptor_owner ret;
     construct_descriptor1(ret);
-    std::cout << *ret << std::endl;
-    
+    std::cout << ret.show() << std::endl;
+
     return 0;
 }
