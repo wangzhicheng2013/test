18043dfac5452ffeb3f14559cdadc35469320c5e
wb7898 <wb7898@arcsoft.com.cn>
2022-03-26 09:39:21 +0800

    12

--- test.cpp
+++ test.cpp
@@ -2,5 +2,10 @@
 #include "boost/date_time/gregorian/gregorian.hpp"
 using namespace boost::gregorian;
 int main() {
+    date d(2022, 3, 6);
+    std::cout << d.year() << std::endl;
+    std::cout << d.month() << std::endl;
+    std::cout << d.day() << std::endl;
+
     return 0;
 }
