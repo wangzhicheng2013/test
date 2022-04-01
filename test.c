726aaf37ae8d7d3e0307f47e7d28b2234ee930f9
wb7898 <wb7898@arcsoft.com.cn>
2022-04-01 10:12:33 +0800

    4

--- test.c
+++ test.c
@@ -1,56 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct _shoes
+typedef struct _Coffee
 {
-    int type;
-    void (*produce_shoes)(struct _shoes *);
-}myShoes;
-void produce_leather_shoes(myShoes *pshoes) 
+    void (*have_coffee)();
+} Coffee;
+typedef struct _Tea
 {
-    if (NULL != pshoes)
-    {
-        printf("produce the leather shoes!\n");
-    }
-}
-void produce_travel_shoes(myShoes *pshoes) 
-{
-    if (NULL != pshoes)
-    {
-        printf("produce the travel shoes!\n");
-    }
-}
-#define LEATHER_TYPE 0x1
-#define TRAVEL_TYPE  0x2
-myShoes *produce_new_shoes(int type)
-{
-    if (type != LEATHER_TYPE && type != TRAVEL_TYPE)
-    {
-        return NULL;
-    }
-    myShoes *pshoes = (myShoes *)malloc(sizeof(myShoes));
-    if (NULL == pshoes) 
-    {
-        return NULL;
+    void (*have_tea)();
+} Tea;
 
-    }
-    pshoes->type = type;
-    if (LEATHER_TYPE == type) 
+void have_black_coffee()
+{
+    printf("have black coffee!\n");
+}
+void have_white_coffee()
+{
+    printf("have white coffee!\n");
+}
+
+void have_red_tea()
+{
+    printf("have red tea!\n");
+}
+void have_green_tea()
+{
+    printf("have green tea!\n");
+}
+
+typedef struct _CoffeeTeaShop
+{
+    Coffee *(*sell_coffee)();
+    Tea *(*sell_tea)();
+} CoffeeTeaShop;
+
+Coffee *sell_black_coffee()
+{
+    Coffee *pCoffee = (Coffee *)malloc(sizeof(Coffee));
+    if (NULL == pCoffee)
     {
-        pshoes->produce_shoes = produce_leather_shoes;
+        return NULL;
+    }
+    pCoffee->have_coffee = have_black_coffee;
+    return pCoffee;
+}
+
+Coffee *sell_white_coffee()
+{
+    Coffee *pCoffee = (Coffee *)malloc(sizeof(Coffee));
+    if (NULL == pCoffee)
+    {
+        return NULL;
+    }
+    pCoffee->have_coffee = have_white_coffee;
+    return pCoffee;
+}
+
+Tea *sell_red_tea()
+{
+    Tea *pTea = (Tea *)malloc(sizeof(Tea));
+    if (NULL == pTea)
+    {
+        return NULL;
+    }
+    pTea->have_tea = have_red_tea;
+    return pTea;
+}
+
+Tea *sell_green_tea()
+{
+    Tea *pTea = (Tea *)malloc(sizeof(Tea));
+    if (NULL == pTea)
+    {
+        return NULL;
+    }
+    pTea->have_tea = have_green_tea;
+    return pTea;
+}
+#define SELL_BLACK_COFFEE_REA_TEA 0x01
+#define SELL_WHITE_COFFEE_GREEN_TEA 0x01
+
+CoffeeTeaShop * create_coffeetea_shop(int type)
+{
+    if (type != SELL_BLACK_COFFEE_REA_TEA && type != SELL_WHITE_COFFEE_GREEN_TEA)
+    {
+        return NULL;
+    }
+    CoffeeTeaShop *pShop = (CoffeeTeaShop *)malloc(sizeof(CoffeeTeaShop));
+    if (NULL == pShop)
+    {
+        return NULL;
+    }
+    if (SELL_BLACK_COFFEE_REA_TEA == type)
+    {
+        pShop->sell_coffee = sell_black_coffee;
+        pShop->sell_tea = sell_red_tea;
     }
     else
     {
-        pshoes->produce_shoes = produce_travel_shoes;
+        pShop->sell_coffee = sell_white_coffee;
+        pShop->sell_tea = sell_green_tea;
     }
-    return pshoes;
+    return pShop;
 }
 int main() 
 {
-    myShoes *pshoes = produce_new_shoes(LEATHER_TYPE);
-    if (pshoes)
+    CoffeeTeaShop *pShop = create_coffeetea_shop(SELL_BLACK_COFFEE_REA_TEA);
+    if (pShop)
     {
-        pshoes->produce_shoes(pshoes);
-        free(pshoes);
+        pShop->sell_coffee();
+        pShop->sell_tea();
+        free(pShop);
     }
 
     return 0;
