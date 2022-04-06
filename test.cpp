ce0638302269cfd4a5c376a3b57014b554fd8aa4
wb7898 <wb7898@arcsoft.com.cn>
2022-04-06 10:48:25 +0800

    6

--- test.cpp
+++ test.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <algorithm>
 #include "boost/array.hpp"
 boost::array<unsigned char, 60> chars_65_125() {
     boost::array<unsigned char, 60>res;
-    boost::algorithm:iota(res.begin(), res.end(), 65);
+    boost::algorithm::iota(res.begin(), res.end(), 65);
     return res;
 }
 int main() {
