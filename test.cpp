db35a80f7bc03d0c9ba46bc003ee1aa613e5cdec
wb7898 <wb7898@arcsoft.com.cn>
2022-04-06 10:44:53 +0800

    6

--- test.cpp
+++ test.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "boost/date_time/gregorian/gregorian.hpp"
-using namespace boost::gregorian;
+#include "boost/array.hpp"
+boost::array<unsigned char, 60> chars_65_125() {
+    boost::array<unsigned char, 60>res;
+    boost::algorithm:iota(res.begin(), res.end(), 65);
+    return res;
+}
 int main() {
-    date d(2022, 3, 6);
-    std::cout << d.year() << std::endl;
-    std::cout << d.month() << std::endl;
-    std::cout << d.day() << std::endl;
-    std::cout << d.week_number() << std::endl;
+    for_each(res.begin(), res.end(), [] (auto e) { std::cout << e << std::endl; } );
 
     return 0;
 }
