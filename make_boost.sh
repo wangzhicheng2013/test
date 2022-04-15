1e6f86f0b3435e23d292a8b5e5a689224e072706
wb7898 <wb7898@arcsoft.com.cn>
2022-04-15 16:58:52 +0800

    12

--- make_boost.sh
+++ make_boost.sh
@@ -1,1 +1,1 @@
-g++ -std=c++17 -g -o Test test.cpp -I ./ -I /opt/boost/boost_1_78_0 -L /opt/boost/boost_1_78_0/stage/lib  -lboost_thread -pthread
+g++ -std=c++17 -g -o Test test.cpp -I ./ -I /opt/boost/boost_1_78_0 -L /opt/boost/boost_1_78_0/stage/lib  -lboost_thread -lboost_chrono -pthread
