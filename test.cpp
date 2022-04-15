3912e3d069f3b63a44d90969633858dac758a055
wb7898 <wb7898@arcsoft.com.cn>
2022-04-15 16:43:48 +0800

    6

--- test.cpp
+++ test.cpp
@@ -3,37 +3,68 @@
 #include <cassert>
 #include <stdexcept>
 #include "boost/scoped_array.hpp"
-#include "boost/move/make_unique.hpp"
-void may_throw1(char) {
+#include "boost/thread.hpp"
+#include "boost/bind.hpp"
+#include "boost/shared_ptr.hpp"
+#include "boost/make_shared.hpp"
+#include "boost/shared_array.hpp"
+void do_process(const char *data, std::size_t size) {
+    assert(size);
+    assert(data);
 }
-void may_throw2(const char *) {
-    throw std::exception();
+void do_process_in_background(const char *data, std::size_t size) {
+    char *data_cpy = new char[size];
+    std::memcpy(data_cpy, data, size);
+    boost::thread(boost::bind(&do_process, data_cpy, size)).detach();
+    boost::thread(boost::bind(&do_process, data_cpy, size)).detach();
 }
-void foo() {
-    char *buffer = new char[1024 * 1024 * 10];
-    may_throw1(buffer[0]);
-    may_throw2(buffer);
-    delete buffer;
+template <std::size_t Size>
+void do_process_shared(const boost::shared_ptr<char[Size]>&data) {
+    data[0] = 1;
+    assert(data[0]);
+    do_process(data.get(), Size);
 }
-void foo_fixed() {
-    boost::scoped_array<char>buffer(new char[1024 * 1024 * 10]);
-    may_throw1(buffer[0]);
-    may_throw2(buffer.get());
+template <std::size_t Size>
+void do_process_in_background_v1(const char *data) {
+    boost::shared_ptr<char[Size]>data_cpy = boost::make_shared<char[Size]>();
+    std::memcpy(data_cpy, data.get(), size);
+    boost::thread(boost::bind(&do_process_shared<Size>, data_cpy)).detach();
+    boost::thread(boost::bind(&do_process_shared<Size>, data_cpy)).detach();
 }
-void foo_fixed2() {
-    const boost::movelib::unique_ptr<char []>buffer = boost::movelib::make_unique<char []>(1024 * 1024 * 10);
-    may_throw1(buffer[0]);
-    may_throw2(buffer.get());
+void do_process_shared_ptr(const boost::shared_ptr<char[]>& data, std::size_t size) {
+    do_process(data.get(), size);
+}
+void do_process_in_background_v2(const char *data, std::size_t size) {
+    boost::shared_ptr<char[]>data_cpy = boost::make_shared<char[]>(size);
+    std::memcpy(data_cpy, data.get(), size);
+    boost::thread(boost::bind(&do_process_shared_ptr<Size>, data_cpy)).detach();
+    boost::thread(boost::bind(&do_process_shared_ptr<Size>, data_cpy)).detach();
+}
+void do_process_shared_ptr2(const boost::shared_ptr<char>&data, std::size_t size) {
+    do_process(data.get(), size);
+}
+void do_process_in_background_v3(const char *data, std::size_t size) {
+    boost::shared_ptr<char>data_cpy(new char[size], boost::checked_array_deleter<char>());
+    std::memcpy(data_cpy, data.get(), size);
+    boost::thread(boost::bind(&do_process_shared_ptr2, data_cpy, size)).detach();
+    boost::thread(boost::bind(&do_process_shared_ptr2, data_cpy, size)).detach();
+}
+void do_process_shared_array(const boost::shared_array<char>&data, std::size_t size) {
+    do_process(data.get(), size);
+}
+void do_process_in_background_v4(const char *data, std::size_t size) {
+    boost::shared_array<char>data_cpy(new char[size]);
+    std::memcpy(data_cpy, data.get(), size);
+    boost::thread(boost::bind(&do_process_shared_array, data_cpy)).detach();
+    boost::thread(boost::bind(&do_process_shared_array, data_cpy)).detach();
 }
 int main() {
-    try {
-        //foo_fixed();
-        foo_fixed2();
-        assert(false);
-    }
-    catch (...) {
+    char ch[] = "Hello dear reader!";
+    do_process_in_background_v1<sizeoif(ch)>(ch);
+    do_process_in_background_v2(ch, sizeof(ch));
+    do_process_in_background_v3(ch, sizeof(ch));
+    do_process_in_background_v4(ch, sizeof(ch));
 
-    }
-
+    boost::this_thread::sleep_for(boost::chrono::seconds(2));
     return 0;
 }
