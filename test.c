2ca0cd2dba51d1031cb7c052ed59ebc22e9b8dd8
wb7898 <wb7898@arcsoft.com.cn>
2022-04-18 08:54:01 +0800

    13

--- test.c
+++ test.c
@@ -1,50 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct _surf_internet 
+typedef struct _FoodClass
 {
-    void (*request)();
-} surf_internet;
-void ftp_request()
+    void (*eat)();
+} FoodClass;
+void eat()
 {
-    printf("request from ftp\n");
+    printf("I want to eat!\n");
 }
-void smtp_request()
+typedef struct _ShopClass
 {
-    printf("request from smtp\n");
+    void (*shop)();
+} ShopClass;
+void shop()
+{
+    printf("I want to shop!\n");
 }
-void web_request()
+typedef struct _PlayClass
 {
-    printf("request from web\n");
+    void (*play)();
+} PlayClass;
+void play()
+{
+    printf("I want to play!\n");
 }
-typedef struct _Proxy
+typedef struct _TourClass
 {
-    surf_internet *client;
-} Proxy;
-void process(Proxy *proxy) 
+    FoodClass *pFood;
+    ShopClass *pShop;
+    PlayClass *pPlay;
+    void (*facade)(struct _TourClass *pTour);
+} TourClass;
+void facade(TourClass *pTour)
 {
-    if ((NULL == proxy) || (NULL == proxy->client))
+    if (NULL == pTour)
     {
         return;
     }
-    proxy->client->request();
+    if (pTour->pFood)
+    {
+        pTour->pFood->eat();
+    }
+    if (pTour->pShop)
+    {
+        pTour->pShop->shop();
+    }
+    if (pTour->pPlay)
+    {
+        pTour->pPlay->play();
+    }
 }
 int main() 
 {
-    Proxy *proxy = (Proxy *)malloc(sizeof(Proxy));
-    if (NULL == proxy)
-    {
-        return -1;
-    }
-    proxy->client = (surf_internet *)malloc(sizeof(surf_internet));
-    if (NULL == proxy->client)
-    {
-        free(proxy);
-        return -1;    
-    }
-    proxy->client->request = web_request;
-    process(proxy);
-    free(proxy->client);
-    free(proxy);
-
+    FoodClass Food;
+    ShopClass Shop;
+    PlayClass Play;
+    Food.eat = eat;
+    Shop.shop = shop;
+    Play.play = play;
+    TourClass Tour;
+    Tour.pFood = &Food;
+    Tour.pShop = &Shop;
+    Tour.pPlay = &Play;
+    Tour.facade = facade;
+    Tour.facade(&Tour);
+    
     return 0;
 }
