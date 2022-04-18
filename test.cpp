e178f19aed3ffb2fc6e0eb4413beebc0acc964b9
wb7898 <wb7898@arcsoft.com.cn>
2022-04-18 11:32:01 +0800

    14

--- test.cpp
+++ test.cpp
@@ -1,71 +1,32 @@
 #include <iostream>
 #include <string>
 #include <cassert>
-#include <stdexcept>
-#include "boost/scoped_array.hpp"
-#include "boost/thread.hpp"
-#include "boost/bind.hpp"
-#include "boost/shared_ptr.hpp"
-#include "boost/make_shared.hpp"
-#include "boost/shared_array.hpp"
-#include "boost/chrono/duration.hpp"
-void do_process(const char *data, std::size_t size) {
-    assert(size);
-    assert(data);
-}
-void do_process_in_background(const char *data, std::size_t size) {
-    char *data_cpy = new char[size];
-    std::memcpy(data_cpy, data, size);
-    boost::thread(boost::bind(&do_process, data_cpy, size)).detach();
-    boost::thread(boost::bind(&do_process, data_cpy, size)).detach();
-}
-template <std::size_t Size>
-void do_process_shared(const boost::shared_ptr<char[Size]>&data) {
-    data[0] = 1;
-    assert(data[0]);
-    do_process(data.get(), Size);
-}
-template <std::size_t Size>
-void do_process_in_background_v1(const char *data) {
-    boost::shared_ptr<char[Size]>data_cpy = boost::make_shared<char[Size]>();
-    std::memcpy(data_cpy.get(), data, Size);
-    boost::thread(boost::bind(&do_process_shared<Size>, data_cpy)).detach();
-    boost::thread(boost::bind(&do_process_shared<Size>, data_cpy)).detach();
-}
-void do_process_shared_ptr(const boost::shared_ptr<char[]>& data, std::size_t size) {
-    do_process(data.get(), size);
-}
-void do_process_in_background_v2(const char *data, std::size_t size) {
-    boost::shared_ptr<char[]>data_cpy = boost::make_shared<char[]>(size);
-    std::memcpy(data_cpy.get(), data, size);
-    boost::thread(boost::bind(&do_process_shared_ptr, data_cpy, size)).detach();
-    boost::thread(boost::bind(&do_process_shared_ptr, data_cpy, size)).detach();
-}
-void do_process_shared_ptr2(const boost::shared_ptr<char>&data, std::size_t size) {
-    do_process(data.get(), size);
-}
-void do_process_in_background_v3(const char *data, std::size_t size) {
-    boost::shared_ptr<char>data_cpy(new char[size], boost::checked_array_deleter<char>());
-    std::memcpy(data_cpy.get(), data, size);
-    boost::thread(boost::bind(&do_process_shared_ptr2, data_cpy, size)).detach();
-    boost::thread(boost::bind(&do_process_shared_ptr2, data_cpy, size)).detach();
-}
-void do_process_shared_array(const boost::shared_array<char>&data, std::size_t size) {
-    do_process(data.get(), size);
-}
-void do_process_in_background_v4(const char *data, std::size_t size) {
-    boost::shared_array<char>data_cpy(new char[size]);
-    std::memcpy(data_cpy.get(), data, size);
-    boost::thread(boost::bind(&do_process_shared_array, data_cpy, size)).detach();
-    boost::thread(boost::bind(&do_process_shared_array, data_cpy, size)).detach();
+#include <algorithm>
+#include "boost/function.hpp"
+typedef boost::function<void(int)>fobject_t;
+class int_processor {
+    const int min_;
+    const int max_;
+    bool &triggered_;
+public:
+    int_processor(int min, int max, bool &triggered)
+        : min_(min)
+        , max_(max)
+        , triggered_(triggered)
+    {}
+    void operator() (int i) const {
+        triggered_ = (i > max_ ) || (i < main_);
+    }
+};
+void process_integers(const fobject_t &f) {
+    static const int data[] = { 1, 2, 3, 4, 200 };
+    std::for_each(begin(data), end(data), f);
 }
 int main() {
-    char ch[] = "Hello dear reader!";
-    do_process_in_background_v1<sizeof(ch)>(ch);
-    do_process_in_background_v2(ch, sizeof(ch));
-    do_process_in_background_v3(ch, sizeof(ch));
-    do_process_in_background_v4(ch, sizeof(ch));
+    bool is_triggered = false;
+    int_processor fo(100, 200, is_triggered);
+    process_integers(fo);
+    std::cout << is_triggered << std::endl;
 
-    boost::this_thread::sleep_for(boost::chrono::seconds(2));
     return 0;
 }
