f55f952f7cb977043d28f688b8810db8585669b1
wb7898 <wb7898@arcsoft.com.cn>
2022-04-20 14:36:14 +0800

    17

--- test.cpp
+++ test.cpp
@@ -1,32 +1,16 @@
 #include <iostream>
 #include <string>
 #include <cassert>
-#include <algorithm>
 #include "boost/function.hpp"
 typedef boost::function<void(int)>fobject_t;
-class int_processor {
-    const int min_;
-    const int max_;
-    bool &triggered_;
-public:
-    int_processor(int min, int max, bool &triggered)
-        : min_(min)
-        , max_(max)
-        , triggered_(triggered)
-    {}
-    void operator() (int i) const {
-        triggered_ = (i > max_ ) || (i < min_);
-    }
-};
-void process_integers(const fobject_t &f) {
-    static const int data[] = { 1, 2, 3, 4, 200 };
-    std::for_each(std::begin(data), std::end(data), f);
+void process_integers(const fobject_t &fun) {
+    fun(10);
+}
+void my_ints_function(int i) {
+    assert(10 == i);
 }
 int main() {
-    bool is_triggered = false;
-    int_processor fo(100, 200, is_triggered);
-    process_integers(fo);
-    std::cout << is_triggered << std::endl;
+    process_integers(&my_ints_function);
 
     return 0;
 }
