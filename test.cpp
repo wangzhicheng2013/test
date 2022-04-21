8baa581eecef8dedc0ed3093c3f1959d79467c52
wb7898 <wb7898@arcsoft.com.cn>
2022-04-21 14:50:13 +0800

    18

--- test.cpp
+++ test.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include <string>
 #include <cassert>
+#include <deque>
+#include <algorithm>
 #include "boost/function.hpp"
 typedef boost::function<void(int)>fobject_t;
 void process_integers(const fobject_t &fun) {
-    fun(10);
+    static const int data[] = { 1, 2, 3, 4, 5, 200 };
+    std::for_each(std::begin(data), std::end(data), fun);
 }
-void my_ints_function(int i) {
-    assert(10 == i);
+void sample() {
+    process_integers([](int) { });
+    std::deque<int>ints;
+    process_integers([&ints](int i) {
+        ints.push_back(i);
+    });
+    std::size_t match_count = 0;
+    process_integers([&ints, &match_count](int i) mutable {
+        if (ints.front() == i) {
+            ++match_count;
+        }
+        ints.pop_front();
+    });
+    assert(6 == match_count);
 }
 int main() {
-    process_integers(&my_ints_function);
+    sample();
+    std::deque<int>v = { 10, 20, 30, 40, 50 };
+    std::for_each(std::begin(v), std::end(v), [](int &v) { v += 10;});
+    const boost::function<void(int&)>f0([] (int &v) { v += 10;});
+    std::for_each(std::begin(v), std::end(v), f0);
 
     return 0;
 }
