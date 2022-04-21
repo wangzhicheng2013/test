dc5d6e454e912e5c3877160453e7a5000619b6c9
wb7898 <wb7898@arcsoft.com.cn>
2022-04-21 10:22:32 +0800

    1

--- test.c
+++ test.c
@@ -1,48 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct _BodyBuilder
+#include <string.h>
+typedef struct _Template
 {
-    void (*play)();
-} BodyBuilder;
-typedef struct _BailPlaying
-{
-    void (*play)();
-} BailPlaying;
-typedef struct _PlayRequest
-{
-    int type;
-    void *pPlaying;
-} PlayRequest;
-#define BODY_BUILDER_TYPE 0
-#define BAIL_PLAYING_TYPE 1
-void person_playing(PlayRequest *pPlayRequest)
-{
-    if ((NULL == pPlayRequest) || (NULL == pPlayRequest->pPlaying))
+    int title;
+    int font;
+    int lineDistance;
+    void (*operate)(struct _Template *pTemplate)
     {
-        return;
+        if (pTemplate)
+        {
+            printf("title:%d, font:%d, lineDistance:%d\n", pTemplate->title, pTemplate->font, pTemplate->lineDistance);
+        }
     }
-    switch (pPlayRequest->type)
+} Template;
+typedef struct _TemplateFactory
+{
+    Template **pTemplates;
+    int num;
+    int size;
+    Template *GetTemplate(struct _TemplateFactory *pTemplateFactory, int title, int font, int lineDistance);
+    void (*free)(struct _TemplateFactory *pTemplateFactory);
+} TemplateFactory;
+Template *GetTemplate(struct _TemplateFactory *pTemplateFactory, int title, int font, int lineDistance)
+{
+    int index = 0;
+    Template *pTemplate = NULL;
+    Template **pTemplates = NULL;
+    if (NULL == pTemplateFactory)
     {
-    case BODY_BUILDER_TYPE:
-        ((BodyBuilder *)pPlayRequest->pPlaying)->play();
-        break;
-    case BAIL_PLAYING_TYPE:
-        ((BailPlaying *)pPlayRequest->pPlaying)->play();
-        break;
+        return NULL;
     }
+    for (;index < pTemplateFactory->num;index++)
+    {
+        if (title != pTemplateFactory->pTemplates[index]->title) 
+        {
+            continue;
+        }
+        if (font != pTemplateFactory->pTemplates[index]->font) 
+        {
+            continue;
+        }
+        if (lineDistance != pTemplateFactory->pTemplates[index]->lineDistance) 
+        {
+            continue;
+        }
+        return pTemplateFactory->pTemplates[index];
+    }
+    pTemplate = (Template *)malloc(sizeof(Template));
+    if (NULL == pTemplate)
+    {
+        return NULL;
+    }
+    pTemplate->title = title;
+    pTemplate->font = font;
+    pTemplate->lineDistance = lineDistance;
+    if (pTemplateFactory->num < pTemplateFactory->size)
+    {
+        pTemplateFactory->pTemplates[index] = pTemplate;
+        ++(pTemplateFactory->num);
+        return pTemplate;
+    }
+    pTemplates = (Template **)malloc(sizeof(Template *) * (2 * pTemplateFactory->size));
+    if (NULL == pTemplates)
+    {
+        return NULL;
+    }
+    memmove(pTemplates, pTemplateFactory->pTemplates, pTemplateFactory->size);
+    free(pTemplateFactory->pTemplates);
+    pTemplateFactory->pTemplates = pTemplates;
+    pTemplateFactory->pTemplates[pTemplateFactory->size] = pTemplate;
+    pTemplateFactory->size *= 2;
+    ++(pTemplateFactory->num);
+    return pTemplate;
 }
-void body_building() 
+void free(struct _TemplateFactory *pTemplateFactory)
 {
-    printf("body building!\n");
+    int index = 0;
+    Template *pTemplate = NULL;
+    for (;index < pTemplateFactory->size;index++)
+    {
+        pTemplate = pTemplateFactory->pTemplates[index];
+        if (pTemplate)
+        {
+            free(pTemplate);
+        }
+    }
+    if (pTemplateFactory->pTemplates)
+    {
+        free(pTemplateFactory->pTemplates);
+    }
 }
 int main() 
 {
-    BodyBuilder bodyBuilder;
-    bodyBuilder.play = body_building;
-    PlayRequest request;
-    request.type = BODY_BUILDER_TYPE;
-    request.pPlaying = &bodyBuilder;
-    person_playing(&request);
+    TemplateFactory templateFactory = { 0 };
+    templateFactory.GetTemplate = GetTemplate;
+    templateFactory.free = free;
+    Template *pTemplate = templateFactory.GetTemplate(&templateFactory, 1, 2, 3);
+    if (pTemplate != NULL)
+    {
+        pTemplate->operate(pTemplate);
+    }
+    templateFactory.free();
 
     return 0;
 }
